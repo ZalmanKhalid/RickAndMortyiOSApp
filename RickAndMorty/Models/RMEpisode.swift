59d9bddf310827ab431f65a5e36e7dc65ecdcf20
Salman Khalid <zalmankhalid@gmail.com>
2023-05-03 22:38:48 +0200

    Added dataModel

--- RickAndMorty/Models/RMEpisode.swift
+++ RickAndMorty/Models/RMEpisode.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+
+// MARK: - RMEpisode
+struct RMEpisode: Codable {
+    let id: Int
+    let name: String
+    let airDate: String
+    let episode: String
+    let characters: [String]
+    let url: String
+    let created: String
+}
+
